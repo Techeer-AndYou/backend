2bd811409d7a537304d59924a09cfd1cca76ae46
ppinkypeach <mira20932@gmail.com>
2023-10-03 19:26:30 +0900

    feat: password 암호화 설정

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Users } from './models/users.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LoginUserResponseDto } from './dto/login-response.dto';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class UsersService {
@@ -13,11 +17,19 @@ export class UsersService {
     ) {}
 
     async createUser(createUserDto: CreateUserDto): Promise<void>{
-        const newUser = this.usersRepository.create(createUserDto);
+        const {email, password, username, phoneNumber} = createUserDto;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = this.usersRepository.create({
+            email,
+            username,
+            phoneNumber,
+            password: hashedPassword
+        });
         await this.usersRepository.save(newUser);
     }
 
-    async loginUser(){
-        
-    }
+
 }
